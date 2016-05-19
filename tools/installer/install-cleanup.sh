2cd0f01661645586f6a0dccfbf3e20e767636e10
Diko <dexter.consunji@elementzinteractive.com>
2016-05-19 15:26:35 +0800

    added extra cleanup for NPM and BOWER modules

--- tools/installer/install-cleanup.sh
+++ tools/installer/install-cleanup.sh
@@ -34,4 +34,41 @@ rm -rf /etc/ssl \
 		/usr/lib/node_modules/npm/doc \
 		/usr/lib/node_modules/npm/html > /dev/null || true
 
+
+#####################################################
+# Purge NPM package directories
+#####################################################
+rm -rf $(find "/usr/lib/node_modules" -name test -o -type d | grep '/test$') 2>/dev/null || true
+
+# cleanup $PROJECT_ROOT/node_modules
+if [ -d "${PROJECT_ROOT}/node_modules" ]; then
+    rm -rf $(find "${PROJECT_ROOT}/node_modules" -name test -o -type d | grep '/test$') 2>/dev/null || true
+fi
+
+# cleanup $PROJECT_ROOT/bower_components
+if [ -d "${PROJECT_ROOT}/bower_components" ]; then
+    rm -rf $(find "${PROJECT_ROOT}/bower_components" -name test -o -type d | grep '/test$') 2>/dev/null || true
+fi
+
+if [ -d "/root/.node-gyp" ]; then
+    rm -rf "/root/.node-gyp" 2>/dev/null || true
+fi
+
+if [ -d "/root/.cache" ]; then
+    rm -rf "/root/.cache" 2>/dev/null || true
+fi
+
+if [ -d "/root/.config" ]; then
+    rm -rf "/root/.config" 2>/dev/null || true
+fi
+
+if [ -d "/root/.local" ]; then
+    rm -rf "/root/.local" 2>/dev/null || true
+fi
+
+if [ -d "/usr/share/doc" ]; then
+    rm -rf "/usr/share/doc" 2>/dev/null || true
+fi
+
+
 exit 0
