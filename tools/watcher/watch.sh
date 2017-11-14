2461da853416b6a4883c3dda49dda6f004f9aa03
Diko <diko316@gmail.com>
2017-11-14 08:46:08 +0800

    fixed file watcher not to kill callback process but move on to the next inotifywait process.

--- tools/watcher/watch.sh
+++ tools/watcher/watch.sh
@@ -22,15 +22,17 @@ trap kill_inotify 6
 trap kill_inotify 15
 
 kill_inotify() {
-    if [ "${APID}" ]; then
-        echo "killing action pid ${APID}"
-        ls /proc | grep '^'${APID}'$' && kill ${APID} && APID=
-    fi
+    kill $(jobs -p) 2>&1 > /dev/null
+    wait
+    # if [ "${APID}" ]; then
+    #     echo "killing action pid ${APID}"
+    #     ls /proc | grep '^'${APID}'$' && kill ${APID} && APID=
+    # fi
 
-    if [ "${PID}" ]; then
-        echo "killing monitor pid ${PID}"
-        ls /proc | grep '^'${PID}'$' && kill ${PID} && PID=
-    fi
+    # if [ "${PID}" ]; then
+    #     echo "killing monitor pid ${PID}"
+    #     ls /proc | grep '^'${PID}'$' && kill ${PID} && PID=
+    # fi
     echo "** End watcher..."
     exit 0
 }
@@ -43,18 +45,18 @@ while true; do
 
     if [ -x "${ACTION}" ]; then
         ${ACTION} &
-        APID=$!
+        #APID=$!
         sleep 1
 
         if [ -d "${FILE}" ]; then
-            inotifywait -q -r -e close_write,modify,move,create,delete "${FILE}" &
+            inotifywait -q -r -e close_write,modify,move,create,delete,attrib,delete_self "${FILE}" 2>&1 > /dev/null
         else
-            inotifywait -q -e close_write,modify,move,create,delete "${FILE}" &
+            inotifywait -q -e close_write,modify,move,create,delete,attrib,delete_self "${FILE}" 2>&1 > /dev/null
         fi
-        PID=$!
-        wait $PID
-        ls /proc | grep '^'${APID}'$' && kill ${APID} && APID=
-        ls /proc | grep '^'${PID}'$' && kill ${PID} && PID=
+        # PID=$!
+        # wait $PID
+        # #ls /proc | grep '^'${APID}'$' && kill ${APID} && APID=
+        # ls /proc | grep '^'${PID}'$' && kill ${PID} && PID=
     else
         echo "!* ${ACTION} is not an executable file."
         break;
